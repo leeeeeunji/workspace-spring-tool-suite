cfe341bae55e60dfe77f660490aeb517c1f6e329
leeeeeunj <qwas2555@naver.com>
2023-03-02 17:55:53 +0900

    3/1

--- spring_web_application/src/main/java/com/itwill/config/WebConfig.java
+++ spring_web_application/src/main/java/com/itwill/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.itwill.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 //@EnableWebMvc
@@ -13,4 +18,28 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("forward:/index.jsp");
 	}
+	
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		//registry.jsp("WEB-INF/views",".jsp");
+	}
+	
+	/***********************spring mvc객체 등록*************************/
+	//ViewResolver등록
+	@Bean
+	public BeanNameViewResolver beanNameViewResolver() {
+		BeanNameViewResolver beanNameViewResolver = new BeanNameViewResolver();
+		beanNameViewResolver.setOrder(0);
+		return beanNameViewResolver;
+	}
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		internalResourceViewResolver.setOrder(1);
+		return internalResourceViewResolver;
+	}
+	
 }
+
+
