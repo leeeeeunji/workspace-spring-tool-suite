2931cb741eaafcee683a466e667fa80761ab7fac
leeeeeunj <qwas2555@naver.com>
2023-03-09 18:25:45 +0900

    3/9

---
+++ javascript_ajax/WebContent/ajaxRequest.js
@@ -0,0 +1,16 @@
+
+function ajaxRequest(method, url, params, callbackFunction) {
+	let xhr=new XMLHttpRequest();
+	method = (method) ? method:'GET';
+	method = (method != 'GET' && method != 'POST')? 'GET':method;
+	params = (params)? params:null;
+	url = (method == 'GET' && params != null)? url + "?" + params:url;
+	
+	xhr.open(method, url, true);
+	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xhr.onload=function() {
+		callbackFunction(xhr);
+	};
+	
+	xhr.send((method == 'POST')? params:null); //get이면 넣고, post면 X
+}
